fb514efa7ae406ff3bd4c0b002e2f696cb75f26f
danheck <danheck@web.de>
2017-02-21 01:03:06 +0100

    0.3.6 new data sets for power pose effect

--- R/data_towels.R
+++ R/data_towels.R
@@ -1,16 +1,17 @@
-#' Data: Reuse of Towels in Hotels
+#' Data Set: Reuse of Towels in Hotels
 #'
-#' Set of studies that investigated how often people reuse towels in hotels (Scheibehenne, Jamil, & Wagenmakers, 2016).
+#' Set of studies that investigated whether people reuse towels in hotels more often if they are provided with a descriptive norm (Scheibehenne, Jamil, & Wagenmakers, 2016).
 #' @details
 #'  Two groups of hotel guests received different messages that encouraged them to reuse their towels. One message simply informed the guests about the benefits of environmental protection (the control condition), and the other message indicated that the majority of guests actually reused their towels in the past (the descriptive-social-norm condition). The results suggested that the latter message facilitated towel reuse.
 #'
-#' @format A data frame 2 variables:
+#' @format A data frame with three variables:
 #' \describe{
-#'   \item{\code{study}}{original study (see Scheibehenne et. al, 2016)}
-#'   \item{\code{logOR}}{log-odds ratio of towel reuse (descriptive-social-norm vs. control)}
-#'   \item{\code{SE}}{standard error of log-odds ratio per study}
+#'   \item{\code{study}}{Authors of original study (see Scheibehenne et. al, 2016)}
+#'   \item{\code{logOR}}{Measure of effect size: log-odds ratio of towel reuse (descriptive-social-norm vs. control)}
+#'   \item{\code{SE}}{Measure of precision: standard error of log-odds ratio per study}
 #' }
-#' @references Scheibehenne, B., Jamil, T., & Wagenmakers, E.-J. (2016). Bayesian Evidence Synthesis Can Reconcile Seemingly Inconsistent Results: The Case of Hotel Towel Reuse. Psychological Science, 27(7), 1043–1046. \url{https://doi.org/10.1177/0956797616644081}
+#' @references
+#' Scheibehenne, B., Jamil, T., & Wagenmakers, E.-J. (2016). Bayesian Evidence Synthesis Can Reconcile Seemingly Inconsistent Results: The Case of Hotel Towel Reuse. Psychological Science, 27(7), 1043–1046. \url{https://doi.org/10.1177/0956797616644081}
 #' @examples
 #' data(towels)
 #' head(towels)
